088730966f1aff18e726a2a279aceb42e8b513c7
adithya-naik <jatothadithyanaik@gmail.com>
2025-03-13 00:05:05 +0530

    added frontend configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Services from "./pages/Services"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+function App() {
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
